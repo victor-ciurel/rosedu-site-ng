fab72ff61ae369719af6115634e3b370f985f177
Bogdan Marius Calapod <bogdan.marius.calapod@gmail.com>
2018-04-21 18:21:15 +0300

    Add header menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,6 +3,11 @@ import PropTypes from 'prop-types'
 
 import Button from 'components/common/Button'
 
+import roseduLogo from './assets/logoWhite.svg'
+import youtube from './assets/youtube.svg'
+import slack from './assets/slack.svg'
+import twitter from './assets/twitter.svg'
+
 import './index.scss'
 
 class Header extends Component {
@@ -12,8 +17,30 @@ class Header extends Component {
   render () {
     return (
       <div className='Header'>
-        <Button> Vezi proiectele noastre </Button>
-        <Button primary> Susține ROSEdu </Button>
+        <header>
+          <img src={roseduLogo} alt='' />
+          <nav>
+            <a href='/'>
+              <span>despre noi</span>
+            </a>
+            <a href='/'>
+              <span>proiecte</span>
+            </a>
+            <a href='/'>
+              <span>contact</span>
+            </a>
+            <a href='/'>
+              <img src={youtube} alt='' />
+            </a>
+            <a href='/'>
+              <img src={twitter} alt='' />
+            </a>
+            <a href='/'>
+              <img src={slack} alt='' />
+            </a>
+          </nav>
+          <Button primary>Susține ROSEdu!</Button>
+        </header>
       </div>
     )
   }
