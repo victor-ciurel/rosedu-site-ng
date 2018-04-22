d0db4c69ac1d2d4c20498734b38cc021c69de44b
Bogdan Marius Calapod <bogdan.marius.calapod@gmail.com>
2018-04-22 23:28:37 +0300

    Add sponsorship section

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import Title from 'components/common/Title'
 
 import Form from './components/Form'
+import Sponsorship from './components/Sponsorship'
 
 import './index.scss'
 
@@ -20,6 +21,7 @@ class Contact extends Component {
         <Title text='contact' color='#23a94b' />
         <div className='content'>
           <Form />
+          <Sponsorship />
         </div>
       </div>
     )
