dda42ffcdddbf95c13f6f554ac661e2b637bc1c4
Teodor Stefu <teodorstefu@gmail.com>
2020-02-17 22:36:48 +0200

    Remove locales

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,14 +7,6 @@
 const fs = require('fs-extra')
 const path = require('path')
 
-exports.onPostBuild = () => {
-  console.log('Copying locales')
-  fs.copySync(
-    path.join(__dirname, '/src/locales'),
-    path.join(__dirname, '/public/locales')
-  )
-}
-
 exports.modifyWebpackConfig = ({ config, stage }) => {
   config.merge({
     resolve: {
